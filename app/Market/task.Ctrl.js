a71c484f8d37558a97bb9c74ae19453b5a650c38
chengwen <81548634@qq.com>
2016-09-01 19:32:17 +0800

    模板修改。

--- app/Market/task.Ctrl.js
+++ app/Market/task.Ctrl.js
@@ -14,180 +14,10 @@ function taskCtrl($scope,$rootScope,$state,PublicResource,$stateParams,NgTablePa
     vm.task.productIds="";
     vm.task.timesLimit="";
     vm.task.amountLimit=""
+    vm.task.storesId=""
     vm.FilterStores = new Array();      //已选择门店
     vm.GoodSpecs = new Array();
     login();
-
-    vm.AddTask = function(){
-        for(var i=0;i<$('.date').length;i++){
-
-            vm.task[$('.date').eq(i).attr('name')] = $('.date').val();
-        }
-        vm.task.storeIds=""
-        if(vm.task.storeType=="SELECTED_STORE"){
-            vm.task.storeIds = ArryString(vm.FilterStores,true)
-        }
-        if(vm.task.exclusive){
-            for(var i in vm.tasklist){
-                
-            }
-        }
-        if(vm.task.productType=='SELECTED_PRODUCT'){
-            vm.task.productIds = ArryString(vm.specs,false)
-        }
-        if(typeof(vm.task.startTime)!="undefined"&&vm.task.startTime!=""&&typeof(vm.task.startTime)!='number'){
-            console.log(typeof(vm.task.startTime))
-            vm.task.startTime = vm.task.startTime.getTime();
-        }
-        if(typeof(vm.task.endTime)!="undefined"&&vm.task.endTime!=""&&typeof(vm.task.endTime)!='number'){
-            console.log(vm.task.endTime)
-            vm.task.endTime = vm.task.endTime.getTime();
-        }
-        console.log(vm.task);
-         MarketResource.add(vm.seid,vm.task).then(function(data){
-            if(data.data.status=="OK"){
-                layer.msg("保存成功",{icon:1})
-            }else{
-                layer.msg(data.data.message,{icon:2})
-            }
-        })
-    }
-
-    //
-    vm.AddStores = function(list){
-        if(list.status){
-            vm.FilterStores.push(list)
-            list.status=false;
-            console.log(vm.FilterStores);
-        }
-    }
-
-    //
-    vm.AddSpecs = function(item){
-        if(item.status){
-            vm.specs.push(item)
-            item.status=false;
-        }
-            vm.specList = new NgTableParams({},{dataset:vm.specs}); 
-    }
-
-    vm.taskBtn = function(){
-        console.log(vm.task)
-    }
-
-    //删除已选择门店
-    vm.delStores = function(i){
-        if(i){
-            vm.FilterStores.splice(i,1)
-            vm.storesList[i].status=true;
-        }else{
-            for(var i in vm.FilterStores){
-                if(vm.FilterStores[i].check){
-                    console.log(vm.storesList[i])
-                    vm.storesList[i].status=true;
-                    vm.storesList[i].active=false;
-                    vm.FilterStores.splice(i,1);
-                }
-            }
-        }
-        
-    }
-
-    //删除已选择规格
-    vm.delSpecs = function(i,id){
-       if(i){
-           for(var x in vm.GoodSpecs){
-                if(vm.GoodSpecs[x].spec.id ==id){
-                        console.log(vm.specs[i])
-                        console.log(vm.GoodSpecs[x])
-                        vm.GoodSpecs[x].status=true;
-                        vm.specs.splice(i,1);
-                        vm.specList = new NgTableParams({},{dataset:vm.specs});
-                    }
-            }
-       }else{
-           for(var i in vm.specs){
-                if(vm.specs[i].check){
-                    console.log(vm.specs[i])
-                    for(var x in vm.GoodSpecs){
-                        if(vm.GoodSpecs[x].spec.id==vm.specs[i].spec.id){
-                            vm.GoodSpecs[x].status=true;
-                        }
-                    }
-                    vm.specs.splice(i,1);
-                }
-            }
-            vm.specList = new NgTableParams({},{dataset:vm.specs});
-       }
-    }
-
-    vm.All = function(is){
-        switch(is){
-            case 'AddStore':
-                for(var i in vm.storesList){
-                        if(vm.storesList[i].active){
-                            vm.storesList[i].active=false;
-                        }else{
-                            vm.storesList[i].active=true;
-                        }
-                    }
-            break;
-            case 'DelStore':
-                for(var i in vm.FilterStores){
-                        if(vm.FilterStores[i].check){
-                            vm.FilterStores[i].check=false;
-                        }else{
-                            vm.FilterStores[i].check=true;
-                        }
-                    }
-            break;
-            case 'AddSpecs':
-                for(var i in vm.GoodSpecs){
-                        if(vm.GoodSpecs[i].active){
-                            vm.GoodSpecs[i].active=false;
-                        }else{
-                            vm.GoodSpecs[i].active=true;
-                        }
-                    }
-            break;
-            case 'DelSpecs':
-                for(var i in vm.specs){
-                        if(vm.specs[i].check){
-                            vm.specs[i].check=false;
-                        }else{
-                            vm.specs[i].check=true;
-                        }
-                    }
-            break;
-        }
-    }
-
-    vm.delAll = function(){
-         for(var i in vm.FilterStores){
-            vm.FilterStores[i].check=true;
-        }
-    }
-
-    vm.TopStore = function(){
-        for(var i in vm.storesList){
-            if(vm.storesList[i].active){
-                vm.FilterStores.push(vm.storesList[i])
-                vm.storesList[i].status=false;
-                vm.storesList[i].active=false;
-            }
-        }
-    }
-
-    vm.SpecsAll = function(){
-        for(var i in vm.GoodSpecs){
-            if(vm.GoodSpecs[i].active){
-                vm.specs.push(vm.GoodSpecs[i])
-                vm.GoodSpecs[i].status=false;
-                vm.GoodSpecs[i].active=false;
-            }
-        }
-        vm.specList = new NgTableParams({},{dataset:vm.specs});
-    }
 
 
     function login() {
@@ -206,18 +36,10 @@ function taskCtrl($scope,$rootScope,$state,PublicResource,$stateParams,NgTablePa
     stores();
     function stores(){
         StoresResource.list(vm.seid,0,0).then(function(data){
-            vm.storesList = data.data.result.data
-            for (var i in vm.storesList) {
-                vm.storesList[i].status=true;
+            vm.stores= data.data.result.data
+            for (var i in vm.stores) {
+                vm.stores[i].status=true;
             }
-            vm.StroesList = new NgTableParams({},{dataset:vm.storesList});   
-        })
-    }
-    tasklist();
-    function tasklist(){
-        MarketResource.list(vm.seid,0,0).then(function(data){
-            vm.tasklist = data.data.result;
-            console.log(vm.tasklist)
         })
     }
 
--- app/Template/Stores.Directive.js
+++ app/Template/Stores.Directive.js
@@ -4,92 +4,86 @@ angular.module('index_area').directive('stores', function (NgTableParams) {
 		replace: true,
 		scope: {
 			list: "=",
-			returnlist:'='
+			returnlist: '='
 		},
 		templateUrl: 'Template/StoresSelect.html',
 		link: function (scope, elem, attr) {
-			for(var i in scope.list){
-				scope.list[i].select=true;
-				scope.list[i].status=false;
+			for (var i in scope.list) {
+				scope.list[i].select = true;
+				scope.list[i].status = false;
 			}
-			scope.returnlist = new Array();
-			scope.tableParams = new NgTableParams({}, { dataset: scope.list });
 
+			scope.select = new Array();
+			
 			//添加门店
-			scope.Add = function(item){
-				if(item){
-					item.select=false;
-					item.status=true;
-					item.active=false;
-					scope.returnlist.push(item);
-				}else{
-					for(var i in scope.list){
+			scope.Add = function (item) {
+				if (item) {
+					item.select = false;
+					item.status = true;
+					item.active = false;
+					scope.select.push(item);
+				} else {
+					for (var i in scope.list) {
 						console.log(scope.list[i])
-						if(scope.list[i].select==true){
-							scope.list[i].select=false;
-							scope.list[i].status=true;
-							scope.list[i].active=false;
-							scope.returnlist.push(scope.list[i])
+						if (scope.list[i].select == true) {
+							scope.list[i].select = false;
+							scope.list[i].status = true;
+							scope.list[i].active = false;
+							scope.select.push(scope.list[i])
 						}
 					}
 				}
-				scope.tableStores = new NgTableParams({},{dataset:scope.returnlist})
 			}
 
-			scope.All = function(is,item){
+			scope.All = function (is, item) {
 				console.log(is)
-					switch(item){
-							case "AddStore":
-								for(var i in scope.list){
-									if(is){
-											scope.list[i].status=true;
-									}else{
-											scope.list[i].status=false;
-									}
-								}
-							break;
-							case "DelStore":
-								for(var i in scope.returnlist){
-									if(is){
-										scope.returnlist[i].active=true;
-									}else{
-										scope.returnlist[i].active=false;
-									}
-								}
-							break;
-					}
+				switch (item) {
+					case "AddStore":
+						for (var i in scope.list) {
+							if (is) {
+								scope.list[i].status = true;
+							} else {
+								scope.list[i].status = false;
+							}
+						}
+						break;
+					case "DelStore":
+						for (var i in scope.select) {
+							if (is) {
+								scope.select[i].active = true;
+							} else {
+								scope.select[i].active = false;
+							}
+						}
+						break;
+				}
 			}
 
-			scope.Del = function(index){
-				if(!index){
-					
-					for(var i in scope.returnlist){
-						if(scope.returnlist[i].active){
-							console.log(scope.returnlist[i])							
-							for(var j in scope.list){
-								if(scope.list[j].id==scope.returnlist[i].id){
-									scope.list[j].status=false;
-									scope.list[j].select=true;
+			scope.Del = function (id,index) {
+				if (false) {
+					for (var i in scope.select) {
+						if (scope.select[i].active) {
+							console.log(scope.select[i])
+							for (var j in scope.list) {
+								if (scope.list[j].id == scope.select[i].id) {
+									scope.list[j].status = false;
+									scope.list[j].select = true;
 								}
 							}
-							scope.returnlist.splice(i,1);
+							scope.select.splice(i,1);
 						}
 					}
-					
-				}else{
+
+				} else {
+					scope.select.splice(index, 1);
 					for(var i in scope.list){
-					if(scope.list[i].id==scope.returnlist[index].id){
+						if(scope.list[i].id==id){
 							scope.list[i].status=false;
 							scope.list[i].select=true;
 						}
 					}
-				scope.returnlist.splice(index,1)
 				}
-				
-				scope.tableStores = new NgTableParams({},{dataset:scope.returnlist})
 			}
-
-			
 
 		}
 	}
